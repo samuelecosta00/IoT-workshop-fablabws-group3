018bfb70171b935d4957ddea5263a9190d25e2f6
samuelecosta00 <87416499+samuelecosta00@users.noreply.github.com>
2021-07-15 11:47:08 +0200

    modifiche sketch
    ulteriori modifiche per la pulizia del codice

--- Files/sketch_ESP32/sketch_ESP32.ino
+++ Files/sketch_ESP32/sketch_ESP32.ino
@@ -5,26 +5,35 @@ extern "C" {
   #include "freertos/timers.h"
 }
 #include <AsyncMqttClient.h>
+
 #define WIFI_SSID "Sam Phone"
 #define WIFI_PASSWORD "1234567890"
+
 #define MQTT_HOST IPAddress(192, 168, 43, 111)
 #define MQTT_PORT 1883
+
 #define MQTT_PUB_TEMP "esp32/dht/temperature"
 #define MQTT_PUB_HUM "esp32/dht/humidity"
+
 #define DHTPIN 4
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
+
 float temp;
 float hum;
+
 unsigned long previousMillis = 0;
 const long interval = 10000;
+
 AsyncMqttClient mqttClient;
 TimerHandle_t mqttReconnectTimer;
 TimerHandle_t wifiReconnectTimer;
+
 void connectToWifi() {
   Serial.println("Connecting to Wi-Fi...");
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 }
+
 void WiFiEvent(WiFiEvent_t event) {
   Serial.printf("[WiFi-event] event: %d\n", event);
     switch(event) {
@@ -41,44 +50,57 @@ void WiFiEvent(WiFiEvent_t event) {
       break;
     }
 }
+
 void connectToMqtt() {
   Serial.println("Connecting to MQTT...");
   mqttClient.connect();
 }
+
 void onMqttPublish(uint16_t packetId) {
   Serial.print("Publish acknowledged.");
   Serial.print(" packetId: ");
   Serial.println(packetId);
 }
+
 void onMqttConnect(bool sessionPresent) {
   Serial.println("Connected to MQTT.");
   Serial.print("Session present: ");
   Serial.println(sessionPresent);
 }
+
 void onMqttPublish(uint16_t packetId) {
   Serial.print("Publish acknowledged.");
   Serial.print(" packetId: ");
   Serial.println(packetId);
 }
+
 void setup() {
   Serial.begin(9600);
   Serial.println();
+  
   dht.begin();
+  
   mqttReconnectTimer = xTimerCreate("mqttTimer",pdMS_TO_TICKS(2000),pdFALSE, (void*)0,reinterpret_cast<TimerCallbackFunction_t>(connectToMqtt));
   wifiReconnectTimer = xTimerCreate("wifiTimer",pdMS_TO_TICKS(2000),pdFALSE, (void*)0,reinterpret_cast<TimerCallbackFunction_t>(connectToWifi));
+  
   WiFi.onEvent(WiFiEvent);
+  
   mqttClient.onConnect(onMqttConnect);
   mqttClient.onDisconnect(onMqttDisconnect);
   mqttClient.onPublish(onMqttPublish);
   mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+  
   connectToWifi();
 }
 void loop() {
   unsigned long currentMillis = millis();
+  
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
+    
     hum = dht.readHumidity();
     temp = dht.readTemperature();
+    
     if (isnan(temp) || isnan(hum)) {
       Serial.println(F("Error: Failed to read from DHT sensor!"));
     } 
