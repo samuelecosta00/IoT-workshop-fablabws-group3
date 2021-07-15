4ecd2ac4bfca584fb28ce19bb4e65dd20717f328
samuelecosta00 <87416499+samuelecosta00@users.noreply.github.com>
2021-07-15 11:42:36 +0200

    modifiche allo sketch
    modifiche di indentazione del codice

--- Files/sketch_ESP32/sketch_ESP32.ino
+++ Files/sketch_ESP32/sketch_ESP32.ino
@@ -1,8 +1,8 @@
 #include "DHT.h";
 #include <WiFi.h>;
 extern "C" {
- #include "freertos/FreeRTOS.h"
- #include "freertos/timers.h"
+  #include "freertos/FreeRTOS.h"
+  #include "freertos/timers.h"
 }
 #include <AsyncMqttClient.h>
 #define WIFI_SSID "Sam Phone"
@@ -22,83 +22,74 @@ AsyncMqttClient mqttClient;
 TimerHandle_t mqttReconnectTimer;
 TimerHandle_t wifiReconnectTimer;
 void connectToWifi() {
- Serial.println("Connecting to Wi-Fi...");
- WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.println("Connecting to Wi-Fi...");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 }
 void WiFiEvent(WiFiEvent_t event) {
- Serial.printf("[WiFi-event] event: %d\n", event);
- switch(event) {
- case SYSTEM_EVENT_STA_GOT_IP:
- Serial.println("WiFi connected");
- Serial.println("IP address: ");
- Serial.println(WiFi.localIP());
- connectToMqtt();
- break;
- case SYSTEM_EVENT_STA_DISCONNECTED:
- Serial.println("WiFi lost connection");
- xTimerStop(mqttReconnectTimer, 0);
- xTimerStart(wifiReconnectTimer, 0);
- break;
- }
+  Serial.printf("[WiFi-event] event: %d\n", event);
+    switch(event) {
+      case SYSTEM_EVENT_STA_GOT_IP:
+        Serial.println("WiFi connected");
+        Serial.println("IP address: ");
+        Serial.println(WiFi.localIP());
+        connectToMqtt();
+      break;
+      case SYSTEM_EVENT_STA_DISCONNECTED:
+        Serial.println("WiFi lost connection");
+        xTimerStop(mqttReconnectTimer, 0);
+        xTimerStart(wifiReconnectTimer, 0);
+      break;
+    }
 }
 void connectToMqtt() {
- Serial.println("Connecting to MQTT...");
- mqttClient.connect();
+  Serial.println("Connecting to MQTT...");
+  mqttClient.connect();
 }
 void onMqttPublish(uint16_t packetId) {
- Serial.print("Publish acknowledged.");
- Serial.print(" packetId: ");
- Serial.println(packetId);
+  Serial.print("Publish acknowledged.");
+  Serial.print(" packetId: ");
+  Serial.println(packetId);
 }
 void onMqttConnect(bool sessionPresent) {
- Serial.println("Connected to MQTT.");
- Serial.print("Session present: ");
- Serial.println(sessionPresent);
+  Serial.println("Connected to MQTT.");
+  Serial.print("Session present: ");
+  Serial.println(sessionPresent);
 }
 void onMqttPublish(uint16_t packetId) {
- Serial.print("Publish acknowledged.");
- Serial.print(" packetId: ");
- Serial.println(packetId);
+  Serial.print("Publish acknowledged.");
+  Serial.print(" packetId: ");
+  Serial.println(packetId);
 }
 void setup() {
- Serial.begin(9600);
- Serial.println();
- dht.begin();
- mqttReconnectTimer = xTimerCreate(
- "mqttTimer",
- pdMS_TO_TICKS(2000),
- pdFALSE, (void*)0,
- reinterpret_cast<TimerCallbackFunction_t>(connectToMqtt)
- );
- wifiReconnectTimer = xTimerCreate(
- "wifiTimer",
- pdMS_TO_TICKS(2000),
- pdFALSE, (void*)0,
- reinterpret_cast<TimerCallbackFunction_t>(connectToWifi)
- );
- WiFi.onEvent(WiFiEvent);
- mqttClient.onConnect(onMqttConnect);
- mqttClient.onDisconnect(onMqttDisconnect);
- mqttClient.onPublish(onMqttPublish);
- mqttClient.setServer(MQTT_HOST, MQTT_PORT);
- connectToWifi();
+  Serial.begin(9600);
+  Serial.println();
+  dht.begin();
+  mqttReconnectTimer = xTimerCreate("mqttTimer",pdMS_TO_TICKS(2000),pdFALSE, (void*)0,reinterpret_cast<TimerCallbackFunction_t>(connectToMqtt));
+  wifiReconnectTimer = xTimerCreate("wifiTimer",pdMS_TO_TICKS(2000),pdFALSE, (void*)0,reinterpret_cast<TimerCallbackFunction_t>(connectToWifi));
+  WiFi.onEvent(WiFiEvent);
+  mqttClient.onConnect(onMqttConnect);
+  mqttClient.onDisconnect(onMqttDisconnect);
+  mqttClient.onPublish(onMqttPublish);
+  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+  connectToWifi();
 }
 void loop() {
- unsigned long currentMillis = millis();
+  unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval) {
- previousMillis = currentMillis;
- hum = dht.readHumidity();
- temp = dht.readTemperature();
- if (isnan(temp) || isnan(hum)) {
- Serial.println(F("Error: Failed to read from DHT sensor!"));
- } else {
- uint16_t packetIdPub1 = mqttClient.publish(MQTT_PUB_TEMP, 1, true, String(temp).c_str()); 
- Serial.printf("Publishing on topic %s at QoS 1, packetId: %i", MQTT_PUB_TEMP, packetIdPub1);
- Serial.printf("Message: %.2f \n", temp);
+    previousMillis = currentMillis;
+    hum = dht.readHumidity();
+    temp = dht.readTemperature();
+    if (isnan(temp) || isnan(hum)) {
+      Serial.println(F("Error: Failed to read from DHT sensor!"));
+    } 
+    else {
+      uint16_t packetIdPub1 = mqttClient.publish(MQTT_PUB_TEMP, 1, true, String(temp).c_str()); 
+      Serial.printf("Publishing on topic %s at QoS 1, packetId: %i", MQTT_PUB_TEMP, packetIdPub1);
+      Serial.printf("Message: %.2f \n", temp);
  
- uint16_t packetIdPub2 = mqttClient.publish(MQTT_PUB_HUM, 1, true, String(hum).c_str()); 
- Serial.printf("Publishing on topic %s at QoS 1, packetId %i: ", MQTT_PUB_HUM, packetIdPub2);
- Serial.printf("Message: %.2f \n", hum);
-}
+      uint16_t packetIdPub2 = mqttClient.publish(MQTT_PUB_HUM, 1, true, String(hum).c_str()); 
+      Serial.printf("Publishing on topic %s at QoS 1, packetId %i: ", MQTT_PUB_HUM, packetIdPub2);
+      Serial.printf("Message: %.2f \n", hum);
+    }
  }
 }
